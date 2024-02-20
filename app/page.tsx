9610c86e1bf256246ad783f634dcfc715533428b
Paloma Borro <paloma.borro@wizeline.com>
2024-02-20 12:52:13 +0100

    Add v0 components and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import { ProductPlayground } from "./ui/purchase_history";
+import { UserForm } from "@/components/user-form";
 
 export default function Home() {
   return (
     <main className="container max-w-lg m-auto flex flex-col min-h-screen">
       <header className="py-4">
-        <h1 className="text-3xl font-bold text-center">SOS</h1>
+        <h1 className="text-3xl font-bold text-center">
+          User profile generation
+        </h1>
       </header>
 
       <div className="flex flex-col gap-y-2">
-        <ProductPlayground />
+        <UserForm />
       </div>
     </main>
   );
