c0a129aef3c1322554b2ced86fceafe0fdf43c81
Joan Ginard <joan.ginard@wizeline.com>
2024-02-20 18:30:48 +0100

    adding form functionality

---
+++ components/ExistingProducts.tsx
@@ -0,0 +1,36 @@
+
+
+export interface Product {
+    id: string;
+    name: string;
+    description: string;
+}
+
+
+export const Products: Product[] = [
+    {
+        id: "1",
+        name: "Wireless Charging Pad",
+        description: "This charging pad offers fast and efficient wireless charging for your devices.",
+    },
+    {
+        id: "2",
+        name: "Smart LED Light Bulb",
+        description: "Control your lighting remotely with this smart LED bulb, compatible with various home automation systems.",
+    },
+    {
+        id: "3",
+        name: "Portable Bluetooth Speaker",
+        description: "Take your music anywhere with this compact and powerful Bluetooth speaker, featuring long battery life.",
+    },
+    {
+        id: "4",
+        name: "Wireless Earbuds",
+        description: "Enjoy high-quality sound and hassle-free listening with these sleek wireless earbuds, perfect for workouts and commuting.",
+    },
+    {
+        id: "5",
+        name: "Smart Thermostat",
+        description: "Save energy and customize your home's temperature with this smart thermostat, accessible from your smartphone.",
+    },
+];
