0797b8d9ed6e3f3417ca82ab0db68e7de132102f
Paloma Borro <paloma.borro@wizeline.com>
2024-02-20 13:15:59 +0100

    Fix page width

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@ import { UserForm } from "@/components/user-form";
 
 export default function Home() {
   return (
-    <main className="container max-w-lg m-auto flex flex-col min-h-screen">
+    <main className="container max-w-[80%] m-auto flex flex-col min-h-screen">
       <header className="py-4">
         <h1 className="text-3xl font-bold text-center">
           User profile generation
         </h1>
       </header>
-
-      <div className="flex flex-col gap-y-2">
-        <UserForm />
-      </div>
+      <UserForm />
     </main>
   );
 }
