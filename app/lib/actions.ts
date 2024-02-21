febc05a0a292fedbecd25c8a6d3ded847e9359c3
Paloma Borro <paloma.borro@wizeline.com>
2024-02-21 13:35:10 +0100

    Remake prompt and export user_description string

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,46 +3,73 @@
 import { PromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
+import { Product, UserInteraction } from "./Database";
+
+type UserInfoServer = {
+  name: string;
+  age: number;
+  productDescriptions: Array<Product["description"]>;
+  userInteraction: Array<UserInteraction["text"]>;
+};
 
 export const generateUserDescriptionFromTheServer: ({
-  products,
-}: {
-  products: string;
-}) => Promise<string> = async ({ products }) => {
-
-
-  return new Promise((resolve) => setTimeout(() => resolve("This person is a tech enthusiast who enjoys cooking and is interested in home automation."), 1000));
-
-  // const chain = PromptTemplate.fromTemplate(
-  //   `You are a personal shopper. Imagine you have to create a user description based on the user purchase history. Generate a brief description about the person who would buy the {products}. What are they like? What do they like? What are they passionate about? Stick to the facts and avoid making assumptions. Example description: "This person is a tech enthusiast who enjoys cooking and is interested in home automation."`
-  // )
-  //   .pipe(
-  //     new ChatOpenAI({
-  //       modelName: "gpt-3.5-turbo",
-  //       temperature: 0.5,
-  //       maxRetries: 2,
-  //     }).bind({
-  //       functions: [
-  //         {
-  //           name: "extractor",
-  //           description: "The user description",
-  //           parameters: {
-  //             type: "object",
-  //             properties: {
-  //               text: {
-  //                 type: "string",
-  //                 description:
-  //                   "The description of the user without formatting in simple text",
-  //               },
-  //             },
-  //             required: ["text"],
-  //           },
-  //         },
-  //       ],
-  //       function_call: { name: "extractor" },
-  //     })
+  productDescriptions,
+  name,
+  age,
+  userInteraction,
+}: UserInfoServer) => Promise<string> = async ({
+  productDescriptions,
+  name,
+  age,
+  userInteraction,
+}) => {
+  const products = productDescriptions.join(". ");
+  const interactions = userInteraction.join(". ");
+  // return new Promise((resolve) =>
+  //   setTimeout(
+  //     () =>
+  //       resolve(
+  //         "This person is a tech enthusiast who enjoys cooking and is interested in home automation."
+  //       ),
+  //     1000
   //   )
-  //   .pipe(new JsonOutputFunctionsParser());
+  // );
+  const chain = PromptTemplate.fromTemplate(
+    `You are a personal shopper. Imagine you have to create a user description based on the user purchase history. The user's name is {name}, age is {age}, and they have bought the following products: ${products}. They have had the following interactions: ${interactions}. Generate a brief description about the person who would buy these products. What are they like? What do they like? What are they passionate about? Stick to the facts and avoid making assumptions. Example description: "This person is a tech enthusiast who enjoys cooking and is interested in home automation."`
+  )
+    .pipe(
+      new ChatOpenAI({
+        modelName: "gpt-3.5-turbo",
+        temperature: 0.5,
+        maxRetries: 2,
+      }).bind({
+        functions: [
+          {
+            name: "extractor",
+            description: "The user description",
+            parameters: {
+              type: "object",
+              properties: {
+                user_description: {
+                  type: "string",
+                  description: "The description of the user",
+                },
+              },
+              required: ["user_description"],
+            },
+          },
+        ],
+        function_call: { name: "extractor" },
+      })
+    )
+    .pipe(new JsonOutputFunctionsParser());
 
-  // return chain.invoke({ products }).then(({ text }) => text);
+  return chain
+    .invoke({
+      productDescriptions: products,
+      name,
+      age: age.toString(),
+      userInteraction: interactions,
+    })
+    .then((result: string) => result);
 };
