e852bc778cbe0d2acb43ff21347f504959a3ec2c
Paloma Borro <paloma.borro@wizeline.com>
2024-02-22 10:57:49 +0100

    Change user interaction text and rewrite prompts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,17 +24,11 @@ export const generateUserDescriptionFromTheServer: ({
   age,
   userInteractions,
 }) => {
-  const products = productDescriptions.map((description) => `* ${description}\n\n`).join();
+  const products = productDescriptions
+    .map((description) => `* ${description}\n\n`)
+    .join();
   const interactions = userInteractions.map((text) => `* ${text}\n\n`).join();
-  // return new Promise((resolve) =>
-  //   setTimeout(
-  //     () =>
-  //       resolve(
-  //         "This person is a tech enthusiast who enjoys cooking and is interested in home automation."
-  //       ),
-  //     1000
-  //   )
-  // );
+
   const chain = PromptTemplate.fromTemplate(
     `You are a personal shopper. Imagine you have to create a user description based on:
 
@@ -45,7 +39,7 @@ export const generateUserDescriptionFromTheServer: ({
     
     The steps to follow are
     1. The above mentioned information will be given to you.
-    2. You will help me to answer the following facts about the user in the following structure. Stick to the facts and avoid making assumptions. Do not summarize information we already know from the input, deduce new information based on you experience.
+    2. You will help me to answer the following facts about the user in the following structure. Stick to the facts and avoid making assumptions. Do not summarize information we already know from the input, deduce new information based on the context we give you.
 
 
     * the user name
@@ -84,18 +78,25 @@ export const generateUserDescriptionFromTheServer: ({
               properties: {
                 user_gender: {
                   type: "string",
-                  description: "reason if the user is a man or a women and why in two sentences",
+                  description:
+                    "reason if the user is a man or a women and why in two sentences. Take in consideration the name gender. Peter is usually a man, Mariah a woman.",
                 },
                 user_basic_interests: {
                   type: "string",
-                  description: "reason of the user basic interests and why in two sentences",
+                  description:
+                    "reason of the user basic interests and why in two sentences based on the purchase history, age and interactions.",
                 },
                 user_communication_style: {
                   type: "string",
-                  description: "reason what is his communication style and why in two sentences",
-                }
+                  description:
+                    "reason what is his communication style and why in two sentences based in interactions. If there are no interactions, just say there is not enogh information.",
+                },
               },
-              required: ["user_gender", "user_basic_interests", "user_communication_style"],
+              required: [
+                "user_gender",
+                "user_basic_interests",
+                "user_communication_style",
+              ],
             },
           },
         ],
@@ -104,11 +105,10 @@ export const generateUserDescriptionFromTheServer: ({
     )
     .pipe(new JsonOutputFunctionsParser());
 
-  return chain
-    .invoke({
-      products,
-      name,
-      age: age.toString(),
-      interactions,
-    });
+  return chain.invoke({
+    products,
+    name,
+    age: age.toString(),
+    interactions,
+  });
 };
