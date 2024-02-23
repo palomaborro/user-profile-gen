1c299a22d2dcf4dbfb0e88ca6093558fc374b26e
Paloma Borro <paloma.borro@wizeline.com>
2024-02-23 16:31:08 +0100

    Update NextAuth configuration to include email verification and Wizeline domain restriction

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,26 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-    ]
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    signIn: async ({ account, profile }) => {
+      if (
+        account.provider === "google" &&
+        profile.email_verified === true &&
+        profile.email.endsWith("@wizeline.com")
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+});
 
-export default handler
+export default handler;
