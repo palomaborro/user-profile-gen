f0381f46308dcf7f1b3118b38e76c66e9b7b9240
Paloma Borro <paloma.borro@wizeline.com>
2024-02-27 16:08:19 +0100

    Add constants for user clustering, basic interests, and communication style

---
+++ app/lib/constants.ts
@@ -0,0 +1,32 @@
+export const USER_CLUSTERING = [
+  "Artists",
+  "Families",
+  "Creative",
+  "Environmentalist",
+  "Homeowners",
+  "Other",
+];
+
+export const USER_BASIC_INTERESTS = [
+  "Cooking",
+  "Music",
+  "Art",
+  "Home",
+  "Kids",
+  "Beauty",
+  "Entertainment",
+  "Parenting",
+  "Education",
+  "Sustainability",
+  "Other",
+];
+
+export const USER_COMMUNICATION_STYLE = [
+  "Respectful",
+  "Angry",
+  "Demanding",
+  "Polite",
+  "Friendly",
+  "Formal",
+  "Other",
+];
